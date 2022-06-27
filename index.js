5b65d10b411b00a6e70b0d9827f63d1cc3db6340
Xhulia Prishka <xh.prishka@gmail.com>
2022-06-27 23:48:25 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+
+This is how an item object should look like
+
+{
+  id: 1, // <- the item id matches the icon name in the assets/icons folder
+  name: "beetroot",
+  price: 0.35 // <- You can come up with your own prices
+}
+
+*/
