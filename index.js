42cdc41d879ec31af734f0c6fbb05a24a6d31c4e
Xhulia Prishka <xh.prishka@gmail.com>
2022-06-30 18:03:15 +0200

    commit

--- index.js
+++ index.js
@@ -9,3 +9,51 @@ This is how an item object should look like
 }
 
 */
+//<li>
+//<div class="store--item-icon">
+//  <img src="assets/icons/001-beetroot.svg" alt="beetroot" />
+//</div>
+//<button>Add to cart</button>
+//</li>
+
+//function listItems(){
+//let liEl= document.createElement('li')
+//let imgDivEl=document.createElement('div')
+//let imgEl=document.createElement('img')
+//imgEl.src="assets/icons/001-beetroot.svg"
+//let buttonEl =document.createElement('button')
+//buttonEl.textContent="Add to cart"
+
+//imgDivEl.append(imgEl)
+//liEl.append(imgDivEl, buttonEl)
+
+//let itemsStoreList=document.querySelector('.item-list')
+//itemsStoreList.append(liEl)
+//console.log(itemsStoreList)
+//}
+
+//listItems()
+
+//recreating the dynamic part
+
+let state={}
+
+function renderStoreItems(){
+  let storeItemsUl=document.querySelector('store--item-list')
+  storeItemsUl.textContent=''
+}
+
+
+function renderCartItems(){
+let cartItemsUl=document.querySelector('cart--item-list')
+cartItemsUl.textContent=''
+}
+
+function renderTotal(){}
+let totalPrice=document.querySelector('total-section')
+
+function render(){
+  renderStoreItems()
+  renderStoreItems()
+  renderTotal()
+}
