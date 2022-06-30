070b387e9cf13ab6d7cf7bec189edf83525d02f2
Xhulia Prishka <xh.prishka@gmail.com>
2022-06-30 18:54:16 +0200

    one item in store list

--- index.js
+++ index.js
@@ -36,24 +36,114 @@ This is how an item object should look like
 
 //recreating the dynamic part
 
-let state={}
+let state = {
+  storeItems: [
+    {
+      id: 1,
+      name: "beetroot",
+      price: 0.35,
+      inCart: 4,
+    },
+    {
+      id: 2,
+      name: "carrot",
+      price: 0.20,
+      inCart: 0,
+    },
+    {
+      id: 3,
+      name: "apple",
+      price: 0.15,
+      inCart: 0,
+    },
+    {
+      id: 4,
+      name: "apricot",
+      price: 0.35,
+      inCart: 0,
+    },
+    {
+      id: 5,
+      name: "avocado",
+      price: 0.75,
+      inCart: 1,
+    },
+    {
+      id: 6,
+      name: "bananas",
+      price: 0.35,
+      inCart: 0,
+    },
+    {
+      id: 7,
+      name: "bell-pepper",
+      price: 0.35,
+      inCart: 0,
+    },
+    {
+      id: 8,
+      name: "berry",
+      price: 0.30,
+      inCart: 7,
+    },
+    {
+      id: 9,
+      name: "blueberry",
+      price: 0.65,
+      inCart: 5,
+    },
+    {
+      id: 10,
+      name: "eggplant",
+      price: 0.25,
+      inCart: 0,
+    },
+  ]
+}
+//update state
+//rerender 
 
-function renderStoreItems(){
-  let storeItemsUl=document.querySelector('store--item-list')
-  storeItemsUl.textContent=''
+//function getTotal(){}
+
+//function increaseQuantity() { }
+
+//function decreaseQuantity() { }
+
+function renderStoreItems() {
+  let storeItemsUl = document.querySelector('.store--item-list')
+  storeItemsUl.textContent = ''
+
+  let liEl = document.createElement('li')
+
+  let imgDivEl = document.createElement('div')
+  imgDivEl.className='.store--item-icon'
+
+  let imgEl = document.createElement('img')
+  imgEl.src = 'assets/icons/001-beetroot.svg'
+
+  let buttonEl = document.createElement('button')
+  buttonEl.textContent = 'Add to cart'
+
+  imgDivEl.append(imgEl)
+  liEl.append(imgDivEl, buttonEl)
+
+  storeItemsUl.append(liEl)
+
 }
 
 
-function renderCartItems(){
-let cartItemsUl=document.querySelector('cart--item-list')
-cartItemsUl.textContent=''
+function renderCartItems() {
+  let cartItemsUl = document.querySelector('cart--item-list')
+  cartItemsUl.textContent = ''
 }
 
-function renderTotal(){}
-let totalPrice=document.querySelector('total-section')
+function renderTotal() { }
+let totalPrice = document.querySelector('total-section')
 
-function render(){
+function render() {
   renderStoreItems()
   renderStoreItems()
   renderTotal()
 }
+
+render()
